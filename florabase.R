cc43c1b1f4e72f142f58ceadc9ae74188ba1ee81
Etienne Laliberté <etienne@MacBook-Pro-de-Etienne.local>
2018-11-30 17:44:38 +0800

    added ALA WA records that were missing in FloraBase

--- florabase.R
+++ florabase.R
@@ -35,3 +35,31 @@ taxon2 <- taxon %>%
 # create csv for import into Fulcrum
 write.csv(taxon2, file = 'florabase_taxon_fulcrum.csv', row.names = F,
           na = '', quote = F)
+
+
+# 30 nov 2018: realised that not all WA taxa are on the list, so downloading all WA plant species from ALA
+wa <- read.csv('records-2018-11-30-2.csv') %>% 
+  rename(taxon_url = Species,
+         scientificName_short = Species.Name,
+         recordedBy = Scientific.Name.Authorship,
+         taxonRank = Taxon.Rank,
+         kingdom = Kingdom,
+         phylum = Phylum,
+         class = Class,
+         order = Order,
+         family = Family,
+         genus = Genus,
+         vernacularName = Vernacular.Name) %>% 
+  mutate(scientificName = paste(scientificName_short, recordedBy))
+
+# filter out those taxa already in taxon2
+wa.sub <- wa %>% 
+  filter(!scientificName %in% as.character(taxon2$scientificName ) )
+nrow(wa) - nrow(wa.sub)
+nrow(wa)
+nrow(wa.sub)
+
+
+# create csv for import into Fulcrum
+write.csv(wa.sub, file = 'wa_taxon_fulcrum.csv', row.names = F,
+          na = '', quote = F)
